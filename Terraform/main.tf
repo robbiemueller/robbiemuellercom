d7a516c430acf9a8e8a7ad939ff35209edb0cce3
phishinginthecloud <robbiemueller310@gmail.com>
2023-04-29 08:41:34 -0500

    api gateway

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -339,3 +339,49 @@ resource "aws_iam_role_policy" "lambda_execution_policy" {
     ]
   })
 }
+
+resource "aws_api_gateway_rest_api" "visitor_counter_api" {
+  name        = "VisitorCounterAPI"
+  description = "API for managing the visitor counter on the resume page."
+}
+
+resource "aws_api_gateway_resource" "visitor_counter_resource" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
+  parent_id   = aws_api_gateway_rest_api.visitor_counter_api.root_resource_id
+  path_part   = "count"
+}
+
+resource "aws_api_gateway_method" "visitor_counter_get" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter_api.id
+  resource_id   = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "visitor_counter_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
+  resource_id = aws_api_gateway_resource.visitor_counter_resource.id
+  http_method = aws_api_gateway_method.visitor_counter_get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "visitor_counter_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
+  stage_name  = "prod"
+
+  depends_on = [
+    aws_api_gateway_integration.visitor_counter_lambda
+  ]
+}
+
+resource "aws_lambda_permission" "apigw_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.visitor_counter_api.execution_arn}/*/*"
+}
